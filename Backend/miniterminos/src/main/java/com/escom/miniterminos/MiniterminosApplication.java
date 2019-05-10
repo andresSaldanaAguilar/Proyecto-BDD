ef25f6e94c8695a48f5e011a4d0359fe92b3ccc8
JuanDanielCR <castilloreyesjuan@gmail.com>
2019-05-10 16:57:16 -0500

    Agregando estructura del proyecto en Spring

---
+++ Backend/miniterminos/src/main/java/com/escom/miniterminos/MiniterminosApplication.java
@@ -0,0 +1,13 @@
+package com.escom.miniterminos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniterminosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniterminosApplication.class, args);
+	}
+
+}
